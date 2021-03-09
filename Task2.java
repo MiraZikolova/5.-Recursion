b9115fad16e55438db7704da6600c588eb4918f1
MiraZikolova <71563228+MiraZikolova@users.noreply.github.com>
2021-03-09 18:26:50 +0200

    5

--- Task2.java
+++ Task2.java
@@ -1,28 +1,34 @@
-package tailRecursion4;
+package generics5;
 
-public class Task2 {
-	public static void main(String[] args) {
-	   	 System.out.println("Sum is " + xMethod(4));
-	}
-// to optimize
-//	public static int xMethod(int n) {
-//	   	 if (n == 1)
-//	   		 return 1;
-//	   	 else
-//	   		 return n + xMethod(n - 1);
-//	}
-	
-//optimized
-	public static int xMethod(int n) {
-	   	 return xMethod(n, 1);
-	}
-	
-	public static int xMethod(int n, int sum) { // example 4,1 : 3,5 : 2,8 
-	   	 if (n == 1)
-	   		 return sum;
-	   	 else {
-	   		 sum += n;                          
-	   		 return xMethod(n - 1, sum);        //then 3,5 : 2,8 : 1,10     answer is 10
-	   	 }
-	}
-}
+import java.util.ArrayList;
+
+//generic stackshte bude array lista
+public class Task2<E> extends ArrayList<E>{                               //directno shte se obrushtame kum metodite kato nashi
+	private java.util.ArrayList <E> list = new java.util.ArrayList<>();
+
+
+
+	//returns the top element, but does not remove it from the stack
+		public E peek() {
+			return get(size()-1);          //size e metod, koito po princip prinadleji na ArrayList i ne e nujno da go predefinirame
+		}
+		
+	//adds a new element on top of the stack
+		public void push(E o) {
+			add(o);                    //sam proverqwa ds
+		}
+		
+	//returns and removes the top element of the stack
+		public E pop() {
+			E o = get(size()-1);
+			remove(size()-1);
+			return o;                    //wrushtame referenciq
+		}
+		
+
+	@Override
+		public String toString() {
+			return "stack: " + super.toString();
+		}
+		}
+
